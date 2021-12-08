b1c3a2d0a8e38bd01306099e2b973dd9c36b67b7
Leo Vásquez Vólquez <vasquezvolquezfelixleonel@gmail.com>
2021-12-07 21:45:58 -0400

    Agregando sidebar de carrito de compras y dándole animaciones con JQuery

--- js/app_index.js
+++ js/app_index.js
@@ -73,7 +73,6 @@ function calculadorIMC()
     else if(pies != '' && pulgadas != '' && peso != '')
     {
       let imc = (librasAKilos / estatura ** 2).toFixed(2);
-      console.log("Escriba, patrón");
       console.log(resultado);
 
       if(pies < 0 && pulgadas < 0 && peso < 0)
@@ -197,9 +196,21 @@ btnResetearFormulario.addEventListener('click', limpiarFormulario);
 
 
 //JQUERY
-//Función para desplegar tabla de IMC debajo de la calculadora de IMC
+//Función para que las demás funciones se ejecuten cuando carga el documento completamente
 $(document).ready(function(){
+  //Función para desplegar tabla de IMC debajo de la calculadora de IMC
   $(".titulo-tabla-imc").click(function(){
     $("table").slideToggle();
+  });
+
+  //Función para desplegar sidebar de carrito de compras
+  $("#btn-cart").click(function(){
+    $(".contenedor-cart").addClass("cart-activo");
+    $(".contenedor-cart").removeClass("cart-oculto");
+  });
+  //Función para ocultar sidebar de carrito de compras
+  $("#btn-cerrar").click(function(){
+    $(".contenedor-cart").removeClass("cart-activo");
+    $(".contenedor-cart").addClass("cart-oculto");
   });
 });
